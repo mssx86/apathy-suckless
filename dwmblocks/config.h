cf8719bd16ff97247d7a30cdd349749aa9afed17
mssx86 <mss@tutanota.de>
2020-04-20 22:12:35 +0300

    add: modified svytx-dwmblocks source.

---
+++ dwmblocks/config.h
@@ -0,0 +1,10 @@
+static const Block blocks[] = {
+	{"", 0,	"~/.config/scripts/bw-read", 1,   0},
+	{"", 0,	"~/.config/scripts/weather", 300, 3},
+	{"", 0,	"~/.config/scripts/mpdstat", 1,   1},
+	{"", 0,	"date '+%a %d %H:%M:%S'",    1,   0},
+	{"", 0,	"~/.config/scripts/uptnmem", 1,   0},
+};
+
+
+static char delim[] = " | ";

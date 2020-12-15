ae80e76daf4c43d2c909c10f5eeaa2ad21a797e0
mssx86 <mss@tutanota.de>
2020-12-15 23:47:32 +0300

    upd: init/config.h, {u,b,w}tmp does not work with musl, point to /dev/null.

--- init/config.h
+++ init/config.h
@@ -2,8 +2,8 @@
 #define ENV_PATH	"/mss/init/bin"
 #define PW_CIPHER	"$6$"	/* SHA-512 */
 #undef UTMP_PATH
-#define UTMP_PATH	"/var/run/utmp"
+#define UTMP_PATH	"/dev/null"
 #undef BTMP_PATH
-#define BTMP_PATH	"/var/log/btmp"
+#define BTMP_PATH	"/dev/null"
 #undef WTMP_PATH
-#define WTMP_PATH	"/var/log/wtmp"
+#define WTMP_PATH	"/dev/null"

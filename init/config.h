5fde6cdb12617982f102f3bcdcc6f9cdc51cdb91
mssx86 <mss@tutanota.de>
2020-12-15 23:21:15 +0300

    add: init/; nuke sbase, take ctrlaltdel+getty+halt+killall5+login+respawn from ubase, add sinit to the mix and merge all under a single makefile.

---
+++ init/config.h
@@ -0,0 +1,9 @@
+#define ENV_SUPATH	"/mss/init/bin"
+#define ENV_PATH	"/mss/init/bin"
+#define PW_CIPHER	"$6$"	/* SHA-512 */
+#undef UTMP_PATH
+#define UTMP_PATH	"/var/run/utmp"
+#undef BTMP_PATH
+#define BTMP_PATH	"/var/log/btmp"
+#undef WTMP_PATH
+#define WTMP_PATH	"/var/log/wtmp"
--- utils/sinit/config.h
+++
@@ -1,5 +0,0 @@
-/* See LICENSE file for copyright and license details. */
-
-static char *const rcinitcmd[]     = { "/mss/init/rc", "init",     NULL };
-static char *const rcrebootcmd[]   = { "/mss/init/rc", "reboot",   NULL };
-static char *const rcpoweroffcmd[] = { "/mss/init/rc", "poweroff", NULL };

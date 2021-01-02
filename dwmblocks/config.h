2f224431d383b27cdcd1e016ca2a0e24ecdd6b7f
mssx86 <mss@tutanota.de>
2021-01-02 03:19:27 +0300

    rmv: dwmblocks, haven't used it in ages.

--- dwmblocks/config.h
+++
@@ -1,8 +0,0 @@
-static const Block blocks[] = {
-	{"", 0,	"~/.config/scripts/weather",  300, 3},
-	{"", 0,	"~/.config/scripts/mpdstat",  1,   1},
-	{"", 0,	"date '+%a %d %H:%M:%S'",     1,   0},
-	{"", 0,	"~/.config/scripts/loadnmem", 1,   0},
-};
-
-static char delim[] = " | ";

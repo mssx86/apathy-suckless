a1fc8492d8466f77aeeef732cb3db7d9e3b5537a
mssx86 <mss@tutanota.de>
2021-04-20 22:59:25 +0300

    rmv: init/, moved to apathy.

--- init/config.h
+++
@@ -1,9 +0,0 @@
-#define ENV_SUPATH	"/mss/init/bin"
-#define ENV_PATH	"/mss/init/bin"
-#define PW_CIPHER	"$6$"	/* SHA-512 */
-#undef UTMP_PATH
-#define UTMP_PATH	"/dev/null"
-#undef BTMP_PATH
-#define BTMP_PATH	"/dev/null"
-#undef WTMP_PATH
-#define WTMP_PATH	"/dev/null"
